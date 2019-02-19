e13218d3f13a1e25f7f469020b3af6c70ea8ca15
André Esteves <andreesteves@Andres-MacBook-Pro.local>
2019-02-19 16:29:29 +0000

    updated code and added comments

--- lab1/src/lab1/Client.java
+++ lab1/src/lab1/Client.java
@@ -3,30 +3,43 @@ package lab1;
 import java.net.*;
 import java.io.*;
 
+/**
+ * Class Client
+ * Usage: javac lab1/client <host_name> <port_number> <oper> <opnd>*
+ *      Example: javac lab1/client localhost 9876 "register 87-UI-64"
+ *      Example: javac lab1/client localhost 9876 "lookup 87-UI-64"
+ * 
+ * Application: Client-server application to manage a small database of license plates.
+ * 				Server must execute in an infinite loop waiting for client requests, processing, and reply to them.
+ *              For any given vehicle, the system should allow to store its plate number and the name of its owner.
+ *              Also, the system should allow to visualize the owner of a given plate number (if exists).
+ * Submites a request to the server, Client waits a reply to the request, prints the reply, and then terminates.
+ */
 public class Client {
-   public static void main(String args[]) throws Exception {
-	   
-	  String hostName = args[0];
-	  Integer portNumber = Integer.parseInt(args[1]);
-	  String oper = args[2];
-	  String opnd = args[3];
-	  
-      DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName(hostName);
-      
-      byte[] sendData = new byte[512];
-      byte[] receiveData = new byte[512];
-      
-      String data = oper + " " + opnd;
-      sendData = data.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, portNumber);
-      clientSocket.send(sendPacket);
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      
-      clientSocket.receive(receivePacket);
-      String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
-      System.out.println("FROM SERVER:" + response);
-      
-      clientSocket.close();
-   }
+   
+    public static void main(String args[]) throws Exception {
+        
+        String hostName = args[0];
+        Integer portNumber = Integer.parseInt(args[1]);
+        String oper = args[2];
+        String opnd = args[3];
+    
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName(hostName);
+        
+        byte[] sendData = new byte[512];
+        byte[] receiveData = new byte[512];
+        
+        String data = oper + " " + opnd;
+        sendData = data.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, portNumber);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        
+        clientSocket.receive(receivePacket);
+        String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("Received from server: " + response);
+        
+        clientSocket.close();
+    }
 }
--- lab1/src/lab1/Server.java
+++ lab1/src/lab1/Server.java
@@ -6,50 +6,59 @@ import java.util.HashSet;
 import java.util.Set;
 import java.io.*;
 
+/**
+ * Class Server
+ * Usage: javac lab1/server <host_name>
+ *      Example: javac lab1/server 9876
+ * 
+ * Application: Client-server application to manage a small database of license plates.
+ * 				Server must execute in an infinite loop waiting for client requests, processing, and reply to them.
+ *              For any given vehicle, the system should allow to store its plate number and the name of its owner.
+ *              Also, the system should allow to visualize the owner of a given plate number (if exists).
+ */
 public class Server {
-   public static void main(String args[]) throws Exception {
+	public static void main(String args[]) throws Exception {
 	   
-	   DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
+		DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
+		
+		System.out.println("Server Running...");
+		byte[] receiveData = new byte[512];
+		byte[] sendData = new byte[512];
+		
+		HashMap<String, String> plateNumbers = new HashMap<String, String>();
 	   
-	   System.out.println("Running");
-	   byte[] receiveData = new byte[512];
-	   byte[] sendData = new byte[512];
-	   
-	   HashMap<String, String> plateNumbers = new HashMap<String, String>();
-	   
-	   
-	   while(true) { 
-          DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-          serverSocket.receive(receivePacket);
-          String sentence = new String( receivePacket.getData(), 0, receivePacket.getLength());
-          System.out.println(sentence);
-          String[] received = sentence.split(" ");
-          
-          if(received[0].compareTo("register") == 0) {
-        	 if(!plateNumbers.containsKey(received[1])) {
-        		 plateNumbers.put(received[1],received[2]);
-        		 sendData = String.valueOf(plateNumbers.size()).getBytes();
-        	 }
-        	 else {
-        		 sendData = "-1".getBytes();
-        	 }
-          }
-          else if(received[0].compareTo("lookup") == 0) {
-        	  String owner = plateNumbers.get(received[1]);
-        	  if(owner != null) {
-        		  sendData = owner.getBytes();
-        	  }
-        	  else {
-        		  sendData = "NOT_FOUND".getBytes();
-        	  }
-          }
-          
-          InetAddress IPAddress = receivePacket.getAddress();
-          int port = receivePacket.getPort();
-          System.out.println(new String(sendData));
-          DatagramPacket sendPacket =
-        		  new DatagramPacket(sendData, sendData.length, IPAddress, port);
-          serverSocket.send(sendPacket);
-	   }
-      }
+	   	while(true) { 
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			serverSocket.receive(receivePacket);
+			String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+			System.out.println("Received packet: " + sentence);
+			String[] received = sentence.split(" ");
+			
+			if(received[0].compareTo("register") == 0) {
+				if(!plateNumbers.containsKey(received[1])) {
+					plateNumbers.put(received[1],received[2]);
+					sendData = String.valueOf(plateNumbers.size()).getBytes();
+				}
+				else {
+					sendData = "-1".getBytes();
+				}
+			}
+			else if(received[0].compareTo("lookup") == 0) {
+				String owner = plateNumbers.get(received[1]);
+				if(owner != null) {
+					sendData = owner.getBytes();
+				}
+				else {
+					sendData = "NOT_FOUND".getBytes();
+				}
+			}
+			
+			InetAddress IPAddress = receivePacket.getAddress();
+			System.out.println("Server sending back: " + new String(sendData));
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receivePacket.getPort());
+			serverSocket.send(sendPacket);
+		}
+
+		System.out.println("Server Stopped!");
+	}
 }
