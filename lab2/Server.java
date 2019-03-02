7d69c77df0a22cd9a9200ba68fb54f6bd60ddb5f
EstevesAndre <estevesandre1@hotmail.com>
2019-03-02 01:21:17 +0000

    started refactoring, added more functions, still in progress

--- lab2/Server.java
+++ lab2/Server.java
@@ -4,12 +4,13 @@ import java.net.*;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Timer;
 
 /**
  * Compile as javac *.java
  * Class Server
- * Usage: java lab2/Server <multicast_address> <multicast_port> <registry_address> <registry_port>
- *      Example: java lab2/Server 230.0.0.0 9876 localhost 4745
+ * Usage: java lab2/Server <srvc_port> <mcast_addr> <mcast_port>
+ *      Example: java lab2/Server 4745 230.0.0.0 9876
  * 
  * Application: Client-server application to manage a small database of license plates.
  * 				Server must execute in an infinite loop waiting for client requests, processing, and reply to them.
@@ -17,34 +18,95 @@ import java.util.regex.Pattern;
  *              Also, the system should allow to visualize the owner of a given plate number (if exists).
  */
 public class Server {
+
+	private InetAddress IPAddress;
+	private Integer portNumber;
+	private Integer servicePort;
+	private MulticastSocket multicastSocket;
+	private DatagramSocket socket;
+	private HashMap<String, String> plateNumbers;
+
+	// time between each advertisement (period)
+    private static final int TIMER_PERIOD = 1000;
+	private static final int MAX_REQUEST_SIZE = 274;
+
+	public Server(String srvcPort, String multicastHostName, String mcastPort) throws Exception {
+		
+		this.plateNumbers = new HashMap<String, String>();
+
+		this.IPAddress = InetAddress.getByName(multicastHostName);
+		this.portNumber = Integer.parseInt(mcastPort);
+		this.servicePort = Integer.parseInt(srvcPort);
+
+		this.socket = new DatagramSocket(this.servicePort);
+		this.multicastSocket = new MulticastSocket(this.portNumber);
+
+		// Creates threads
+			// 1 for loop of advertisements
+			// other to recieve requests and response to them
+		//Timer timer  = new Timer();
+		//timer.scheduleAtFixedRate(null, 0, TIMER_PERIOD);
+		
+		System.out.println("Created server!!");
+	}
+
+	private void receiveRequest() throws Exception {
+		byte[] receiveData = new byte[MAX_REQUEST_SIZE];
+		DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
+
+		this.socket.receive(packet);
+		String received = new String(packet.getData(), 0, packet.getLength());
+		System.out.println("Received packet: " + received);
+
+		String[] receivedSplited = received.split(" ");
+		switch (receivedSplited[0]) {
+            case "REGISTER":
+                //sendReply(registerUser(groups[1], received.substring(OWNER_NAME_POS)));
+                break;
+            case "LOOKUP":
+                //sendReply(getOwner(groups[1]));
+                break;
+            default:
+                System.err.println("Server Error: Received unknown request.");
+        }
+
+		byte[] sendData = new byte[MAX_REQUEST_SIZE];
+	}
+
+	private static boolean validatePlateNumber(String platen) throws Exception {
+		Pattern pattern = Pattern.compile("(\\w\\w-){2}\\w{2}");
+		Matcher matcher = pattern.matcher(platen);
+
+		return matcher.matches();
+	}
+
+	private void sendAdvertisement() throws Exception {
+		// sends advertisement
+		// "IP"+" "+"Port"
+		byte[] advertisementData = new byte[MAX_REQUEST_SIZE];
+		advertisementData = (this.IPAddress.getHostName() + " " + this.servicePort).getBytes();	
+		
+		DatagramPacket advertisementPacket = new DatagramPacket(advertisementData, advertisementData.length, this.IPAddress, this.portNumber);
+		this.multicastSocket.send(advertisementPacket);
+
+		System.out.println("Advertisement sent!");
+	}
+
 	public static void main(String args[]) throws Exception {
 	   
-		if(args.length != 4)
+		if(args.length != 3)
 		{
-			System.out.println("Wrong number of arguments.\nUsage: java lab2/Server <multicast_address> <port_number> <IP_registry> <registry_port>");
+			System.out.println("Wrong number of arguments.\nUsage: java lab2/Server <srvc_port> <mcast_addr> <mcast_port>");
 			System.exit(-1);
 		}
 		
-		Integer portNumber = Integer.parseInt(args[1]); 
+		Server server = new Server(args[0], args[1], args[2]);
+		
+		server.sendAdvertisement();
 
-		InetAddress IPAddress = InetAddress.getByName(args[0]);
-		MulticastSocket multiSocket = new MulticastSocket(portNumber);
-
-		byte[] sendAdvertisement = new byte[512];
-		// "IP"+" "+"Port"
-		sendAdvertisement = (InetAddress.getByName(args[2]).getHostName() + " " + args[3]).getBytes();
-
-		while(true)
-		{	
-			DatagramPacket advertisement = new DatagramPacket(sendAdvertisement, sendAdvertisement.length, IPAddress, portNumber);
-			multiSocket.send(advertisement);
-
-			//break;
-		}
-
-		//multiSocket.close();
+		server.multicastSocket.close();
         
-		/*System.out.println("Server Running...");
+		/*
 		byte[] receiveData = new byte[512];
 		byte[] sendData = new byte[512];
 		
@@ -96,13 +158,5 @@ public class Server {
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receivePacket.getPort());
 			serverSocket.send(sendPacket);
 		}*/
-	}
-
-	private static boolean validatePlateNumber(String platen)
-	{
-		Pattern pattern = Pattern.compile("(\\w\\w-){2}\\w{2}");
-		Matcher matcher = pattern.matcher(platen);
-
-		return matcher.matches();
 	}
 }
