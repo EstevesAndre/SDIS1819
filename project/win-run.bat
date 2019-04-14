ff21abe11dd8609f4ba3cce4d0325b7b828557cc
EstevesAndre <estevesandre1@hotmail.com>
2019-04-14 21:58:36 +0100

    updated windows scripts

--- project/win-run.bat
+++ project/win-run.bat
@@ -1,9 +1,10 @@
 @echo off
 
-SET nPeers=%1
+SET v=%1
+SET nPeers=%2
 CD ./bin
 
 FOR /l %%x IN (1, 1, %nPeers%) DO (
-    START cmd.exe /k "java project/service/Peer 1.0 %%x RemoteInterface%%x "230.0.0.0 9876" "230.0.0.1 9877" "230.0.0.2 9878""
+    START cmd.exe /k "java project/service/Peer %v% %%x RemoteInterface%%x "230.0.0.0 9876" "230.0.0.1 9877" "230.0.0.2 9878""
 )
 PAUSE
